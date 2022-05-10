4fc3f4f2efe517f5dd43da294f12e2e9da67c298
liuhanfei <liuhanfei@baiwang.com>
2022-05-10 09:29:21 +0800

    build: 配置eslint,prettier,stylelint;使用less css预处理

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  /**
+   * 插件配置
+   */
+  pluginOptions: {
+    // 添加了插件(@samhammer/vue-cli-plugin-stylelint), 所以需要配置
+    lintStyleOnBuild: true,
+    stylelint: {
+      fix: true, // boolean (default: true)
+    },
+    // 注入less全局变量
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [path.resolve(__dirname, './src/assets/style/variables/*.less')],
+    },
+  },
+});
