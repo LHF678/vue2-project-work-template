9e08ad8d2422fe104495bc09f5df85eb41d5665f
liuhanfei <liuhanfei@baiwang.com>
2023-11-29 18:35:25 +0800

    feat(root): 引入store状态管理

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import stor from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  stor,
   render: (h) => h(App),
 }).$mount('#app');
